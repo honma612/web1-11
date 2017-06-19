a4c0fb12b252eb2de488deeddcbb7693c3581f95
honma612 <oshiri_hakase@yahoo.co.jp>
2017-06-19 17:45:16 +0900

    Update default.js

--- default.js
+++ default.js
@@ -13,6 +13,28 @@ while(ans != num){
   }
 }
 
+Click(){
+  var target = docment.getElementById("out");  
+  var select = docment.getElementById("sel");
+  switch(select.selectedIndex){
+    case 0:
+      target.innerHTML = "ありがとう<br/>";
+      break;
+    case 1:
+      target.innerHTML = "よかった<br/>";
+      break;
+    case 2:
+      target.innerHTML = "うん<br/>";
+      break;
+    case 3:
+      target.innerHTML = "ごめんね<br/>";
+      break;
+    case 4:
+      target.innerHTML = "ゆるして<br/>";
+      break;
+                             }
+}
+
 var mes;
 mes = 'あたり';
 
