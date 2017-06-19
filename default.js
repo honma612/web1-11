cb4837fff11bedf2ff545de7cc1c21d4d69e15e0
honma612 <oshiri_hakase@yahoo.co.jp>
2017-06-19 17:22:19 +0900

    Update default.js

--- default.js
+++ default.js
@@ -3,19 +3,18 @@ var ans= parseInt(window.prompt('0~10の数字を入力'));
 var mes;
 
 while(ans != num){
-;
-if(ans === num){
-  mes = 'あたり';
-}
-else if(ans > num){
-  ans = parseInt(window.prompt('もっと小さいです\n0~10の数字を入力'));
-}
-else if(ans < num){
-  ans = parseInt(window.prompt('もっと大きいです\n0~10の数字を入力'));
-}
-else{
-  ans = parseInt(window.prompt('数字を入力してください\n0~10の数字を入力'));
-}
+  if(ans === num){
+    mes = 'あたり';
+  }
+  else if(ans > num){
+    ans = parseInt(window.prompt('もっと小さいです\n0~10の数字を入力'));
+  }
+  else if(ans < num){
+    ans = parseInt(window.prompt('もっと大きいです\n0~10の数字を入力'));
+  }
+  else{
+    ans = parseInt(window.prompt('数字を入力してください\n0~10の数字を入力'));
+  }
 }
 
 document.getElementById('choice').textContent = mes;
